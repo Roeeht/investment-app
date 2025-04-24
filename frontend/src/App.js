38bbe4286cc620a12e39f6486973d2ace464411a
Roee Habari Tamir <roeehabaritamir@gmail.com>
2025-04-24 18:16:03 +0300

    added card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,30 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+const Card = ({ title }) => {
+  return (
+    <div className='card'>
+      <h1>{title}</h1>
+      <button></button>
+    </div>
+  );
+};
+
 function App() {
-    const [message, setMessage] = useState("");
+  const [title, setTitle] = useState("");
 
-    useEffect(() => {
-        axios.get("http://127.0.0.1:8000/").then(response => {
-            setMessage(response.data.message);
-        });
-    }, []);
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/").then((response) => {
+      setTitle(response.data.message);
+    });
+  }, []);
 
-    return (
-    
-    <h1>{message}</h1>
-)}
+  return (
+    <div className='card-container'>
+      <h1>{title}</h1>
+      <Card title='Hi' />
+    </div>
+  );
+}
 
 export default App;
