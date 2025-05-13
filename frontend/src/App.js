6df402d413570edddc8a7f1c8d3224eabe80df89
Roeeht <roeehabaritamir@gmail.com>
2025-05-13 23:10:35 +0300

    cleanup of docker and db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,28 @@ import axios from "axios";
 
 const Card = ({ title }) => {
   const [added, setAdded] = useState(false);
+  const [stock, setStock] = useState({});
+
   useEffect(() => {
-    console.log(`${title} has been ${added}`);
-  }, [added]);
+    console.log(`Has ${title} been added? ${added}`);
+  }, [title, added]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://financialmodelingprep.com/stable/quote?symbol=^GSPC&apikey=4cDgO8QI4anGo1puGjCsXj0xRS62Tiyd"
+      )
+      .then((response) => {
+        setStock(response);
+        console.log(response);
+      });
+  }, []);
 
   return (
-    <div className='card'>
+    <div className="card">
       <h1>{title}</h1>
-      <button className='button' onClick={() => setAdded(!added)}>
+      <h2>Stock : {JSON.stringify(stock?.data?.[0]?.name || "Loading...")} </h2>
+      <button className="button" onClick={() => setAdded(!added)}>
         {added ? "added" : "not added"}
       </button>
     </div>
@@ -19,19 +33,11 @@ const Card = ({ title }) => {
 
 function App() {
   const [title, setTitle] = useState("");
-  useEffect(() => {
-    axios.get("http://127.0.0.1:8000/").then((response) => {
-      setTitle(response.data.message);
-    });
-  }, []);
 
   return (
-    <div className='card-container'>
+    <div className="card-container">
       <h1>{title}</h1>
-      <Card title='Stock Name1' />
-      <Card title='Stock Name2' />
-      <Card title='Stock Name3' />
-      <Card title='Stock Name4' />
+      <Card title="s&p500" />
     </div>
   );
 }
